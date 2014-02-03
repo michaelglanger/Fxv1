ca541c99bc0323d7b3c8a8fd0870af0c781a7269
Michael Langer <michaelgerd.langer@agfa.com>
2014-02-03 12:05:21 +0100

    first commit netbeans

---
+++ Fxv1/src/net/mgl/oanda/api/v1/Fxv1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.mgl.oanda.api.v1;
+
+/**
+ *
+ * @author axjyb
+ */
+public class Fxv1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
